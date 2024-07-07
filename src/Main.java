fa764d29437429f95d4c9c0e1e9a963ad8114e47
AslanJavasky <aslanbolur@gmail.com>
2024-07-07 11:24:33 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Main {
+/**
+ * @author Aslan Javasky
+ * @version 1.0.2025
+ */
+
+//this Main class
+public class Main {
+    //this method "main" is entry point to our program
+    public static void main(String[] args) {
+
+        /*
+        this instruction
+        print string
+        with greeting
+         */
+        System.out.println("My name is Aslan!");
+
+    }
 }

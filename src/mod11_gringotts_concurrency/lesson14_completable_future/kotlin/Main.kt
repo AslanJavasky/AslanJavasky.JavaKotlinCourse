835c4521af4f90264109ed522c8e29dbc5914036
Aslan Javasky <71359713+AslanJavasky@users.noreply.github.com>
2024-11-25 12:00:16 +0300

    gringotts concurrency+virtual threads

--- src/mod11_gringotts_concurrency/lesson14_completable_future/kotlin/Main.kt
+++ src/mod11_gringotts_concurrency/lesson14_completable_future/kotlin/Main.kt
@@ -1,10 +1,9 @@
-package mod11_gringotts_concurrency.lesson14_completable_future.kotlin
+package mod11_gringotts_concurrency.lesson15_virtual_threads.kotlin
 
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.Executors
 import java.util.concurrent.ForkJoinPool
 import java.util.concurrent.Future
-import java.util.concurrent.TimeUnit
 
 const val CNT_TRANSFERS = 200
 
@@ -14,37 +13,54 @@ fun main() {
     val hagridAccount = BankAccount("Hadrid's account", 500_000)
     val ollivanderAccount = BankAccount("Ollivander's account", 15_000)
 
-    val executor=Executors.newVirtualThreadPerTaskExecutor()
+    //Future
+//    val executor = Executors.newFixedThreadPool(2)
+//    val future1= executor.submit {
+//        repeat(100) {
+//            hagridAccount.transferFunds(ollivanderAccount, 10)
+////            Transfer(hagridAccount,ollivanderAccount,10).execute()
+//        }
+//    }
+//    val future2= executor.submit {
+//        repeat(100) {
+//            Transfer(hagridAccount,ollivanderAccount,10).execute()
+//        }
+//    }
+//    future1.get()
+//    future2.get()
+//
+//    executor.shutdown()
+//
+//    println(
+//        "Balances after transfers:" + hagridAccount.accountName + ":" + hagridAccount.balance + ";" +
+//                ollivanderAccount.accountName + ":" + ollivanderAccount.balance
+//    )
 
-    executor.submit {
-        doTransactions(hagridAccount, ollivanderAccount)
+    //CompletableFuture
+    val executor = Executors.newFixedThreadPool(2)
+    val future1= CompletableFuture.runAsync {
+        repeat(100) {
+            hagridAccount.transferFunds(ollivanderAccount, 10)
+//            Transfer(hagridAccount,ollivanderAccount,10).execute()
+        }
+    }
+    val future2= CompletableFuture.runAsync {
+        repeat(100) {
+            Transfer(hagridAccount,ollivanderAccount,10).execute()
+        }
     }
 
-    executor.submit {
-        doTransactions(hagridAccount, ollivanderAccount)
-    }
+    val allOf=CompletableFuture.allOf(future1,future2)
+
+    val totalFuture=allOf.thenRunAsync {
+        println(
+            "Balances after transfers:" + hagridAccount.accountName + ":" + hagridAccount.balance + ";" +
+                    ollivanderAccount.accountName + ":" + ollivanderAccount.balance
+        )
+    }.join()
 
     executor.shutdown()
 
-    executor.awaitTermination(60, TimeUnit.SECONDS)
-    
-    println(
-        "Balances after transfers:" + hagridAccount.accountName + ":" + hagridAccount.balance + ";" +
-                ollivanderAccount.accountName + ":" + ollivanderAccount.balance
-    )
 
 
-
-
-
-
-}
-
-private fun doTransactions(
-    hagridAccount: BankAccount,
-    ollivanderAccount: BankAccount
-) {
-    repeat(100) {
-        hagridAccount.transferFunds(ollivanderAccount, 10)
-    }
 }
